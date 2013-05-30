4ae0056a09edc23b830f1a0f0e75a6e8c09438e0
ClundXIII <simon.m.m@freenet.de>
2013-05-30 20:42:10 +0200

    ...

---
+++ src/main/game/entities/DirectionHelper.java
@@ -0,0 +1,35 @@
+package main.game.entities.controls;
+
+import com.jme3.math.Vector3f;
+
+public class DirectionHelper{
+
+	//The X, Y, and Z Position in Space
+	private double Position_X;
+	private double Position_Y;
+	private double Position_Z;
+
+	//The Rotation
+	//Rotation "around" [...] Axis:
+	private double Rotation_X;
+	private double Rotation_Y;
+	private double Rotation_Z;
+
+	//the Movement:
+	private double Movement_X;
+	private double Movement_Y;
+	private double Movement_Z;
+
+	public Vector3f getPosition() {
+
+	}
+	
+	public Vector3f getRotation() {
+
+	}
+	
+	public Vector3f getMovement() {
+
+	}
+	
+}
--- src/main/game/entities/Spacecraft.java
+++ src/main/game/entities/Spacecraft.java
@@ -1,5 +1,6 @@
 package main.game.entities;
 
 public class Spacecraft {
-
+	protected SpacecraftControl Control;
+	
 }
