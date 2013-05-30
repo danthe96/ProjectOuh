1e42d30c9b55f1731b2cd89c6ac1d02857b07e9f
danthe <danthe96@gmx.de>
2013-05-30 14:18:25 +0200

    First dummy

---
+++ src/mainpackage/Main.java
@@ -0,0 +1,13 @@
+package mainpackage;
+
+import com.jme3.app.SimpleApplication;
+
+public class Main extends SimpleApplication{
+
+	@Override
+	public void simpleInitApp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
