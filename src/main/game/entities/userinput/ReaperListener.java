dc698c02d7eca9cb2ec9a15be790849afb937a0f
danthe <danthe96@gmx.de>
2013-05-31 16:19:45 +0200

    Changed ReaperListener

--- src/main/game/entities/userinput/ReaperListener.java
+++ src/main/game/entities/userinput/ReaperListener.java
@@ -3,38 +3,43 @@ package main.game.entities.userinput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
 
-
-
 public class ReaperListener extends SpacecraftListener {
+	
+	public ReaperListener(){
+		
+	}
 
-	public static ActionListener actionListener = new ActionListener() {
+	public ActionListener actionListener = new ActionListener() {
 		public void onAction(String name, boolean keyPressed, float tpf) {
-			if(name.equals("ACCELERATE")&&keyPressed)
+			if (name.equals("ACCELERATE") && keyPressed) {
 				spacecraft.accelerate();
-			else if(name.equals("DECELERATE")&&keyPressed)
+			} else if (name.equals("DECELERATE") && keyPressed) {
 				spacecraft.decelerate();
-			else if(name.equals("RIGHT")&&keyPressed);
-			else if(name.equals("LEFT")&&keyPressed);
-		}
-	};
+			} else if (name.equals("RIGHT") && keyPressed) {
 
-	public static AnalogListener analogListener = new AnalogListener() {
-		public void onAnalog(String name, float value, float tpf) {
-			switch(name){
-			case "ROTATE_RIGHT":
-				spacecraft.rightRotation();
-				break;
-			case "ROTATE_LEFT":
-				spacecraft.leftRotation();
-				break;
-			case "STEER_UP":
-				spacecraft.upRotation();
-				break;
-			case "STEER_DOWN":
-				spacecraft.downRotation();
-				break;
+			} else if (name.equals("LEFT") && keyPressed) {
+
 			}
 		}
 	};
+
+	public AnalogListener analogListener = new AnalogListener() {
+		public void onAnalog(String name, float value, float tpf) {
+				if(name.equals("ROTATE_RIGHT") && value > 0){
+					spacecraft.rightRotation();
+				}
+				else if(name.equals("ROTATE_LEFT") && value > 0){
+					spacecraft.leftRotation();
+				}
+				else if(name.equals("STEER_UP") && value > 0){
+					spacecraft.upRotation();
+				}
+				else if (name.equals("STEER_DOWN") && value > 0){
+					spacecraft.downRotation();
+				}
+			}
+	};
+	
+	
 
 }
