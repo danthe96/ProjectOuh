6e1cad739430cc8ab5fb1aac77837cf3c81b600d
Simon Michalke <simon.m.m@freenet.de>
2013-06-30 12:17:11 +0200

    added message recognizing

--- src/main/game/net/netlistener.java
+++ src/main/game/net/netlistener.java
@@ -1,5 +1,8 @@
 package main.game.net;
 
+import main.game.net.utils.messagetype;
+import main.game.net.utils.movement;
+
 import com.jme3.network.Client;
 import com.jme3.network.Message;
 import com.jme3.network.MessageListener;
@@ -20,9 +23,30 @@ public class netlistener implements MessageListener<Client> {
 	}
 	
 	@Override
-	public void messageReceived(Client arg0, Message arg1) {
-		// TODO Auto-generated method stub
-		
+	public void messageReceived(Client source, Message messageInput) {
+	    if (messageInput instanceof message) {
+			message Input = (message) messageInput;
+			System.out.println("Client #" + source.getId() + ":");
+	    	if (Input.getMessagetype() == messagetype.movement){
+	    		movement newPos = (movement) Input.getInformation();
+	    		System.out.println("Position: " + newPos.getPosition());
+	    		System.out.println("Rotation: " + newPos.getRotation());
+	    		System.out.println("Speed:    " + newPos.getSpeed());
+	    	}
+	    	else if (Input.getMessagetype() == messagetype.ping){
+	    		
+	    	}
+	    	else if (Input.getMessagetype() == messagetype.spawn){
+	    		
+	    	}
+	    	else if (Input.getMessagetype() == messagetype.game){
+	    		
+	    	}
+	    	else
+	    		System.out.println("unknown messagetype!");
+	      }
+	    else
+	    	System.out.println("unknown Message instance!");
 	}
 
 }
